f48add54e4f9d54972d3a3401b86ab1c50a2cd6f
portal martin <ptl_martin@yahoo.co.jp>
2024-05-04 02:26:27 +0900

    home dashboard implementation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firsName: "John",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firsName || "guest"}
+            subtext="to your online banking"
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,19 @@
+const HeaderBox = ({
+  type = "title",
+  title,
+  subtext,
+  user,
+}: HeaderBoxProps) => {
+  return (
+    <div className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        {type === "greeting" && (
+          <span className="text-bankGradient"> {user}</span>
+        )}
+      </h1>
+    </div>
+  );
+};
+
+export default HeaderBox;
